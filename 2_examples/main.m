0ed6b866f51cb5eacfdcbf6303de00438eb53ca7
moffmofu <mofumori@icloud.com>
2023-11-05 16:41:55 +0900

    dev:2_examples/main.mにパス通し文がなかったので追加

--- 2_examples/main.m
+++ 2_examples/main.m
@@ -5,6 +5,8 @@
 
 dataDir="../******"; % file path to your directory
 dataName="*******.csv"; % file name
+addpath("../0_sources/common"); % path to utils 
+addpath("../0_sources/core"); % path to myanalysis
 
 data=MyAnalysis(dataDir,dataName);
 data.overView();
